282ccf43df6a21e7900621760bf99e0322de199c
max <max.nick.demax@gmail.com>
2023-10-24 19:35:50 +0300

    temp

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -1,11 +1,21 @@
 package course.concurrency.m2_async.cf.min_price;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class PriceAggregator {
 
     private PriceRetriever priceRetriever = new PriceRetriever();
+
+    private ExecutorService executor = Executors.newCachedThreadPool(); // to handle all blocking requests
 
     public void setPriceRetriever(PriceRetriever priceRetriever) {
         this.priceRetriever = priceRetriever;
@@ -19,6 +29,18 @@ public class PriceAggregator {
 
     public double getMinPrice(long itemId) {
         // place for your code
-        return 0;
+        List<CompletableFuture<Double>> futures = shopIds.stream()
+                .map(id -> CompletableFuture.supplyAsync(() -> priceRetriever.getPrice(itemId, id), executor)
+                        .completeOnTimeout(Double.POSITIVE_INFINITY, 2800, TimeUnit.MILLISECONDS)
+                        .exceptionally(ex -> Double.POSITIVE_INFINITY))
+                .collect(Collectors.toList());
+        CompletableFuture
+                .allOf(futures.toArray(CompletableFuture[]::new))
+                .join();
+        return futures.stream()
+                .mapToDouble(CompletableFuture::join)
+                .filter(Double::isFinite)
+                .min()
+                .orElseGet(() -> Double.NaN);
     }
 }
