69cba3fd21704190a41f12c69f6502bd7bad7d32
max <max.nick.demax@gmail.com>
2023-10-28 08:02:27 +0300

    add PingPong

--- src/main/java/course/concurrency/m3_shared/PingPong.java
+++ src/main/java/course/concurrency/m3_shared/PingPong.java
@@ -1,13 +1,43 @@
 package course.concurrency.m3_shared;
 
 public class PingPong {
+    public static volatile boolean isPing = true;
+    private static final Object lock = new Object();
 
     public static void ping() {
-
+        try {
+            while (true) {
+                synchronized (lock) {
+                    if (!isPing) {
+                        lock.wait();
+                    }
+                    System.out.println("Ping");
+                    Thread.sleep(1000);
+                    isPing = false;
+                    lock.notify();
+                }
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void pong() {
-
+        try {
+            while (true) {
+                synchronized (lock) {
+                    if (isPing) {
+                        lock.wait();
+                    }
+                    System.out.println("Pong");
+                    Thread.sleep(1000);
+                    isPing = true;
+                    lock.notify();
+                }
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
